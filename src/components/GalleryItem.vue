cb8817789f4dfdefd2deef5edb6396dc54c43b15
Vansh <vipultalwin99@gmail.com>
2025-08-26 01:04:07 +0530

    new dist

--- src/components/GalleryItem.vue
+++ src/components/GalleryItem.vue
@@ -4,11 +4,14 @@ defineProps({
     type: Object
   }
 })
+const getImageUrl = (path) => {
+    return new URL(`../../public/pins/${path}`, import.meta.url).href;
+};
 </script>
 
 <template>
   <div class="w-[90%]">
-    <img :src="item.media" class="rounded-lg m-4 w-full h-auto"/>
+    <img :src="getImageUrl(item.media)" class="rounded-lg m-4 w-full h-auto"/>
   </div>
 </template>
 
